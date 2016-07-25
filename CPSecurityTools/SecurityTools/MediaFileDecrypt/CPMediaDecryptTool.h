972fa1bb23823ded316d586c15ce944b77a99125
Wade <391565320@qq.com>
2016-07-25 21:43:10 +0800

    完善功能

--- CPSecurityTools/SecurityTools/MediaFileDecrypt/CPMediaDecryptTool.h
+++ CPSecurityTools/SecurityTools/MediaFileDecrypt/CPMediaDecryptTool.h
@@ -14,17 +14,37 @@ typedef enum : NSUInteger {
     CPMediaDecryptTypeMP4,  // MP4类型
     CPMediaDecryptTypeAVI   // AVI类型
 } CPMediaDecryptType;
-
+/**
+ *  进度block回调
+ *
+ *  @param progress 整体进度不是一个文件的解密进度
+ */
 typedef void(^CPDecryptMediaFileProgressBlock)(float progress);
 
+/**
+ *  解密完成后回调的block
+ *
+ *  @param error 如果为空则解密成功
+ */
 typedef void(^CPDecryptMediaFileComplitionBlock)(NSError *error);
 
 @interface CPMediaDecryptTool : NSObject
 
-
-
+/**
+ *  初始化工具类
+ *
+ *  @return 工具类
+ */
 + (instancetype)defaultDecryptTool;
-
+/**
+ *  解密当前文件夹及文件夹下的所有音/视频文件
+ *
+ *  @param filePath   当前文件目录
+ *  @param header     salt头长度
+ *  @param key        加密的Key值
+ *  @param progress   进度block
+ *  @param complition 完成回调block
+ */
 - (void)decryptWithsuperFilePath:(NSString *)filePath  headerLength:(NSString *)header keyValue:(NSString *)key progress:(CPDecryptMediaFileProgressBlock)progress complition:(CPDecryptMediaFileComplitionBlock)complition;
 
 
--- CPSecurityTools/SecurityTools/MediaFileDecrypt/CPMediaDecryptTool.m
+++ CPSecurityTools/SecurityTools/MediaFileDecrypt/CPMediaDecryptTool.m
@@ -23,11 +23,21 @@
 // 当前解密的文件长度
 @property (nonatomic, assign) NSInteger  currnentLength;
 
+// 线程控制
+@property (nonatomic, strong) NSOperationQueue *oprationQ;
 
 @end
 
 
 @implementation CPMediaDecryptTool
+
+- (NSOperationQueue *)oprationQ{
+    if (!_oprationQ) {
+        _oprationQ = [[NSOperationQueue alloc] init];
+        _oprationQ.maxConcurrentOperationCount = 2;
+    }
+    return _oprationQ;
+}
 
 
 + (instancetype)defaultDecryptTool{
@@ -38,6 +48,15 @@
     return tool;
 }
 
+/**
+ *  解密目录下所有的音视频文件
+ *
+ *  @param filePath   文件目录
+ *  @param header     解密头
+ *  @param key        解密码
+ *  @param progress   进度block
+ *  @param complition 完成调用的block
+ */
 - (void)decryptWithsuperFilePath:(NSString *)filePath headerLength:(NSString *)header keyValue:(NSString *)key progress:(CPDecryptMediaFileProgressBlock)progress complition:(CPDecryptMediaFileComplitionBlock)complition
 {
     NSFileManager *manager = [NSFileManager defaultManager];
@@ -45,38 +64,178 @@
     NSMutableArray *temp = [NSMutableArray array];
     for (NSString *subPath in arr) {
         if ([self mediaTypeWithFilePath:subPath]) {
-          NSString  *mediaPath = [filePath stringByAppendingPathComponent:subPath];
-         [temp addObject:mediaPath];
+            NSString  *mediaPath = [filePath stringByAppendingPathComponent:subPath];
+            [temp addObject:mediaPath];
         }
     }
     
-   __block NSInteger successNum = 0;
-    for (NSString *mediapath in temp)
+    //    NSLog(@"dsadsa");
+    
+    __block NSInteger successNum = 0;
+    for (int i = 0;i < temp.count;i++)
     {
-        NSOperationQueue  *oprationQ = [[NSOperationQueue alloc] init];
-        oprationQ.maxConcurrentOperationCount = 3;
+        NSString *mediaPath = temp[i];
+        
         NSBlockOperation *opration = [NSBlockOperation blockOperationWithBlock:^{
-            [self decryptMediaFilePieceWithFilePath:mediapath header:header key:key complition:^(NSError *error) {
-                NSLog(@"解密完成--%@",error);
-                successNum ++;
-                if (progress) {
-                    float prog = (float)successNum / temp.count;
-                    progress(prog);
-                }
-                if (temp.count == successNum) {
-                    NSError *error;
+            if ([self mediaTypeWithFilePath:mediaPath]) {
+                BOOL isSuc = [self addDecryptFileWithMediaPath:mediaPath index:i header:header.integerValue key:key.integerValue];
+                NSLog(@"解密--%d",isSuc);
+                if (isSuc) {
+                    successNum++;
+                    if (successNum == temp.count) {
+                        NSError *error;
+                        complition(error);
+                    }else{
+                        float pro = (float)successNum / temp.count;
+                        progress(pro);
+                    }
+                }else{
+                    NSError *error = [NSError errorWithDomain:@"解密文件失败..." code:101 userInfo:@{@"filePath":[filePath stringByAppendingPathComponent:mediaPath]}];
                     complition(error);
+                    return;
                 }
-            }];
-            
+            }
         }];
-        [oprationQ addOperation:opration];
+        
+        [self.oprationQ addOperation:opration];
     }
 }
 
 
 /**
- *  解密当个文件的方法
+ *  解密视音频文件
+ *
+ *  @param mediaPath 视音频文件路径
+ *  @param index     数组中文件路径下标
+ *
+ *  @return 是否解密成功
+ */
+- (BOOL)addDecryptFileWithMediaPath:(NSString *)mediaPath index:(NSInteger)index  header:(NSInteger)header key:(NSInteger)key{
+    
+    NSFileManager *manager = [NSFileManager defaultManager];
+    
+    NSString *subPath = mediaPath;
+    // 文件类型
+    NSString *pathExtension = [subPath pathExtension];
+    
+    // 视音频文件路径的前路径
+    NSString *prePath = [subPath stringByDeletingLastPathComponent];
+    
+    // 当前文件名
+    NSString *tempfilePath = [NSString stringWithFormat:@"%@/ios_temp_%ld.%@",prePath,index,pathExtension];
+    [manager removeItemAtPath:tempfilePath error:nil];
+    [manager createFileAtPath:tempfilePath contents:nil attributes:nil];
+    
+    // 创建读写FILE
+    FILE *readFile;
+    FILE *writeFile;
+    readFile = fopen([subPath cStringUsingEncoding:NSUTF8StringEncoding], "rb+");
+    writeFile = fopen([tempfilePath cStringUsingEncoding:NSUTF8StringEncoding], "a+");
+    
+    
+    NSDictionary *attributes = [manager attributesOfItemAtPath:subPath error:nil];
+    
+    _fileLength = ((NSNumber *)[attributes objectForKey:@"NSFileSize"]).integerValue;
+    
+    int decodeKey = (int)key;
+    if (self.fileLength > header) {
+        self.currnentLength = header;
+        BOOL isSuc =  decodeFile(readFile, self.fileLength, self.currnentLength, writeFile,decodeKey);
+        if (isSuc) {
+            if([manager removeItemAtPath:mediaPath error:nil]){
+                isSuc =  [manager moveItemAtPath:tempfilePath toPath:mediaPath error:nil];
+            }else{
+                isSuc = NO;
+            }
+        }
+        return isSuc;
+    }else{
+        return NO;
+    }
+}
+
+
+/**
+ *  C函数解密视频
+ *
+ *  @param file         待解密的文件File
+ *  @param fileLength   文件长度
+ *  @param currentLegth 当前解密的文件长度
+ *  @param direcFile    目标存储目录
+ */
+BOOL decodeFile(FILE *file,long fileLength,long currentLegth,FILE *direcFile,int key){
+    BOOL isSucessed;
+    
+    NSInteger readLength = 1024 * 1024 * 10;
+    if (fileLength - currentLegth > readLength) {
+        // 指针指向首字节
+        //读取文件
+        // 成功，返回0，失败返回-1
+        int set = fseek(file, currentLegth, SEEK_SET);
+        if (set == 0) {
+            Byte *buffer = malloc(sizeof(Byte) * readLength);
+            fread(buffer, readLength, sizeof(Byte), file);
+            currentLegth += readLength;
+            //设置读取位置
+            
+            printf("递归-fseek--%d\n",set);
+            for (int i = 0 ; i < readLength; i++) {
+                *(buffer + i ) -= key;
+            }
+            
+            size_t writedData =  fwrite(buffer, sizeof(Byte), readLength, direcFile);
+            if (writedData == readLength) {
+                isSucessed = YES;
+            }else{
+                isSucessed = NO;
+            }
+            free(buffer);
+            if (isSucessed) {
+                return  decodeFile(file, fileLength, currentLegth,direcFile,key);
+            }else{
+                return isSucessed;
+            }
+            // 递归
+        }
+    }else{
+        if(fileLength - currentLegth  > 0){
+            long length = fileLength - currentLegth;
+            int set = fseek(file, currentLegth, SEEK_SET);
+            if (set == 0) {
+                Byte *buffer = malloc(sizeof(Byte) * length);
+                
+                fread(buffer, length, sizeof(Byte), file);
+                
+                currentLegth += length;
+                //设置读取位置
+                for (int i = 0 ; i < length; i++) {
+                    *(buffer + i ) -= key;
+                }
+                
+                size_t writedData = fwrite(buffer, sizeof(Byte), length, direcFile);
+                
+                printf("最终-fseek--%d",set);
+                if (writedData == length) {
+                    isSucessed = YES;
+                }else{
+                    isSucessed = NO;
+                }
+                
+                // 释放内存
+                free(buffer);
+                
+                // 关闭文件
+                fclose(file);
+                fclose(direcFile);
+                
+            }
+        }
+    }
+    return isSucessed;
+}
+
+/**
+ *  解密当个文件的方法 <不可用 性能差>
  *
  *  @param filePath 单个文件的路径
  */
@@ -87,7 +246,7 @@
         // 文件类型
         NSString *pathExtension = [filePath pathExtension];
         
-//        filePath = [filePath stringByDeletingPathExtension];
+        //        filePath = [filePath stringByDeletingPathExtension];
         
         NSString *currentName = [filePath lastPathComponent];
         filePath = [filePath stringByDeletingLastPathComponent];
@@ -110,9 +269,9 @@
         isSuccess =  [data writeToFile:tempfilePath atomically:YES];
         if (isSuccess) {
             NSFileManager *fileManage = [NSFileManager defaultManager];
-           BOOL isDelete =  [fileManage removeItemAtPath:filePath error:nil];
+            BOOL isDelete =  [fileManage removeItemAtPath:filePath error:nil];
             if (isDelete) {
-               isSuccess = [fileManage moveItemAtPath:tempfilePath toPath:filePath error:nil];
+                isSuccess = [fileManage moveItemAtPath:tempfilePath toPath:filePath error:nil];
             }
         }
     }
@@ -121,7 +280,7 @@
 
 
 /**
- *  分段式解密视频目录
+ *  分段式解密视频目录  <不可用 性能差>
  *
  *  @param filePath 文件路径
  *  @param key      解密Key
@@ -156,7 +315,7 @@
     NSString *pathExtension = [filePath pathExtension];
     
     //filePath = [filePath stringByDeletingPathExtension];
-     NSString *fileComponentPath = [filePath stringByDeletingLastPathComponent];
+    NSString *fileComponentPath = [filePath stringByDeletingLastPathComponent];
     
     NSString *directPath = [NSString stringWithFormat:@"%@/ios_temp.%@",fileComponentPath,pathExtension];;
     
@@ -169,7 +328,7 @@
 }
 
 /**
- *  视频文件递归解密
+ *  视频文件递归解密  <不可用 性能差>
  *
  *  @param fileHandle   文件Hangle
  *  @param fileLength    文件长度
@@ -184,8 +343,8 @@
         [fileHandle seekToFileOffset:self.currnentLength];
         self.currnentLength += readLength;
         NSData *data =  [fileHandle readDataOfLength:readLength];
-//        Byte *readByte1 = (Byte *)[data bytes];
- 
+        //        Byte *readByte1 = (Byte *)[data bytes];
+        
         Byte *readByte = malloc(sizeof(Byte) * readLength);
         
         [data getBytes:readByte length:sizeof(Byte) * readLength];
@@ -207,9 +366,9 @@
     }else{
         NSFileManager *manger = [NSFileManager defaultManager];
         if (self.fileLength - self.currnentLength > 0) {
-             [fileHandle seekToFileOffset:self.currnentLength];
+            [fileHandle seekToFileOffset:self.currnentLength];
             NSData *data =  [fileHandle readDataOfLength:self.fileLength - self.currnentLength];
-             Byte *readByte = (Byte *)[data bytes];
+            Byte *readByte = (Byte *)[data bytes];
             for(int i = 0 ;i < [data length]; i++ )
             {
                 *(readByte + i) -= key.integerValue;
@@ -220,9 +379,9 @@
             [fileHandle closeFile];
             data = nil;
             [manger removeItemAtPath:filePath error:nil];
-          
+            
             BOOL isSuccess = [[NSFileManager defaultManager] moveItemAtPath:directPath toPath:filePath error:nil];
-             NSError *error;
+            NSError *error;
             if (isSuccess) {
                 self.singleComplitionBlock(error);
             }else{
@@ -231,14 +390,14 @@
             }
             self.currnentLength = self.fileLength;
             
-           
+            
         }else{
             // 结束
             [writeHandle closeFile];
             [fileHandle closeFile];
             
             [manger removeItemAtPath:filePath error:nil];
-           BOOL isSuccess = [manger moveItemAtPath:directPath toPath:filePath error:nil];
+            BOOL isSuccess = [manger moveItemAtPath:directPath toPath:filePath error:nil];
             NSError *error;
             if (isSuccess) {
                 self.singleComplitionBlock(error);
@@ -267,7 +426,7 @@
     }else if([filePath hasSuffix:@".AVI"]||[filePath hasSuffix:@".avi"]){
         return CPMediaDecryptTypeAVI;
     }else {
-          return CPMediaDecryptTypeNone;
+        return CPMediaDecryptTypeNone;
     }
 }
 
